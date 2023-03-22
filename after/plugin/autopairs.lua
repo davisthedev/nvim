231d9558e2711b2bebc8fe4311b9e8620cb08cf2
Davis Sanders <dsanders@smartlink.city>
2023-03-22 13:34:03 -0400

    formatting

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -25,5 +25,5 @@ local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done {
-    map_char = { tex = "" }
+  map_char = { tex = "" }
 })
--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,17 +1,18 @@
-require('lualine').setup{
+require('lualine').setup {
   options = {
     theme = 'auto',
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff'},
-    lualine_c = {'buffers'},
-    lualine_x = {'tabs'},
-    lualine_y = {'progress'},
+    lualine_a = { 'mode' },
+    lualine_b = { 'branch', 'diff' },
+    lualine_c = { 'buffers' },
+    lualine_x = { 'tabs' },
+    lualine_y = { 'progress' },
     lualine_z = {
-      { 'diagnostics',
-        sources = {'nvim_diagnostic', 'nvim_lsp'},
-        sections = {'error', 'warn', 'info', 'hint'},
+      {
+        'diagnostics',
+        sources = { 'nvim_diagnostic', 'nvim_lsp' },
+        sections = { 'error', 'warn', 'info', 'hint' },
         diagnostics_color = {
           -- Same values as the general color option can be used here.
           error = 'DiagnosticError', -- Changes diagnostics' error color.
@@ -19,7 +20,7 @@ require('lualine').setup{
           info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
           hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
         },
-        symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+        symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' },
         colored = true,           -- Displays diagnostics status in color if set to true.
         update_in_insert = false, -- Update diagnostics in insert mode.
         always_visible = false,   -- Show diagnostics even if there are none.
@@ -27,4 +28,3 @@ require('lualine').setup{
     }
   }
 }
-
