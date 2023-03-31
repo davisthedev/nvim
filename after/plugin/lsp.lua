43f3c347f368ff8953ca08c7c34917acd6ae61bc
Davis Sanders <dsanders@smartlink.city>
2023-03-31 16:16:27 -0400

    add eslint_d code actions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -153,6 +153,7 @@ mason_lspconfig.setup_handlers {
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 local sources = {
   formatting.prettierd.with({
@@ -160,6 +161,9 @@ local sources = {
   }),
   formatting.stylua,
   diagnostics.eslint_d.with({
+    filetypes = { "svelte", "typescript", "javascript", "vue" }
+  }),
+  code_actions.eslint_d.with({
     filetypes = { "svelte", "typescript", "javascript", "vue" }
   })
 }
