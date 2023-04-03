d9381d558fa49b6ec51b0be0702dedeac25743d2
Davis Sanders <dsanders@smartlink.city>
2023-04-03 11:31:11 -0400

    add template string plugin

--- lua/setup/packer.lua
+++ lua/setup/packer.lua
@@ -64,6 +64,22 @@ require('packer').startup(function(use)
   }
 
   use {
+    'axelvc/template-string.nvim',
+    config = function()
+      require('template-string').setup {
+        filetypes = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue', 'svelte' },
+        remove_template_string = true,
+        restore_quotes = {
+          normal = [[']],
+          jsx = [["]],
+        },
+      }
+    end,
+    event = 'InsertEnter',
+    ft = { 'typescript', 'javascript', 'typescriptreact', 'javascriptreact', 'vue', 'svelte' },
+  }
+
+  use {
     "jose-elias-alvarez/null-ls.nvim",
     requires = { "nvim-lua/plenary.nvim" },
   }
