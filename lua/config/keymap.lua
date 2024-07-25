ef31ed41f4223d1ab2408bcc4bbb4a143abb7dd9
Davis Sanders <davis@roveiq.com>
2024-07-25 13:20:39 -0400

    fix harpoon and split hopping

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -14,7 +14,7 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<leader>v', '<cmd>vsplit<cr>', { desc = 'Split window vertically' })
+vim.keymap.set('n', '<leader>wv', '<cmd>vsplit<cr>', { desc = 'Split window vertically' })
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
