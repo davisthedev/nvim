295b8e944ed7f1381b1e6e1ffb37bacbb5e3f025
Davis Sanders <davisthedev@protonmail.com>
2024-09-16 12:15:48 -0400

    add undotree and diff.exe

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,3 +1,7 @@
+if vim.fn.has "win32" == 1 then
+  vim.opt.shell = "pwsh"
+  vim.g.undotree_DiffCommand = vim.fn.stdpath "config" .. "\\bin\\diff.exe"
+end
 require('config.options')
 require('config.keymap')
 require('config.autocmd')
