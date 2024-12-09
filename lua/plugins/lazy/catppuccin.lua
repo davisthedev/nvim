9e9a1f490f80bb15a6939d741e80dd55b96fdcdb
Davis Sanders <davisthedev@protonmail.com>
2024-12-09 09:44:32 -0500

    use catppuccin

---
+++ lua/plugins/lazy/catppuccin.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup()
+    end,
+    init = function()
+      vim.cmd [[colorscheme catppuccin-mocha]]
+    end,
+  }
+}
